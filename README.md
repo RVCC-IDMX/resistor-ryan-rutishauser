7ebf3f87736455a6e8c0f7c612987fb426919c76
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-11-20 20:36:43 -0500

    Make resistor display interactive

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Resistor
+This program allows you to interact with each of the colors on the resistor band to figure out the values that will be produced in any configuration chosen.
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import { getResistorOhms } from './resistor.js';
+
+const calc = document.querySelector('.calculator');
+const ceramic = document.querySelector('.ceramic');
+const answer = document.querySelector('.answer');
+//
+
+calc.addEventListener('click', (evt) => {
+  if (!evt.path[1].id.includes("color")) {
+    return;
+  }
+  const colorPosition = evt.path[1].id[5];
+  const cl = ceramic.children[colorPosition]
+  cl.classList.replace(cl.classList[1], evt.path[0].innerHTML)
+  const colors = [
+    ceramic.children[0].classList[1], ceramic.children[1].classList[1],
+    ceramic.children[2].classList[1], ceramic.children[3].classList[1]
+  ]
+  answer.innerText = getResistorOhms(colors);
+});
