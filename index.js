527aea6ecc07ea47158a21fafb6862205f19b73c
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-11-20 20:55:03 -0500

    Add composed path

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const answer = document.querySelector('.answer');
 //
 
 calc.addEventListener('click', (evt) => {
-  if (!evt.path[1].id.includes("color")) {
+  if (!evt.composedPath()[1].id.includes("color")) {
     return;
   }
-  const colorPosition = evt.path[1].id[5];
+  const colorPosition = evt.composedPath()[1].id[5];
   const cl = ceramic.children[colorPosition]
-  cl.classList.replace(cl.classList[1], evt.path[0].innerHTML)
+  cl.classList.replace(cl.classList[1], evt.composedPath()[0].innerHTML)
   const colors = [
     ceramic.children[0].classList[1], ceramic.children[1].classList[1],
     ceramic.children[2].classList[1], ceramic.children[3].classList[1]
